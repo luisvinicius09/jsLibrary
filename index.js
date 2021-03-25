558e87d7a68680c3dcd84c6c7428a47edb88a509
realisticattorney <g.axl.aquila@gmail.com>
2021-03-25 17:57:28 -0300

    Add examples of objects

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-const div = document.querySelector('body');
-div.style.backgroundColor = 'blue'
+// let myLibrary = [];
+
+// function Book() {
+//   // the constructor...
+// }
+
+// function addBookToLibrary() {
+//   // do stuff here
+// }
+
+
+
+let hobbit = {
+  title: 'The Hobbit',
+  author: 'Tolkien',
+  pages: 1200,
+  read: false
+}
+
+let potter = {
+  title: 'Harry Potter',
+  author: 'some lady',
+  pages: 600,
+  read: false
+}
+
+let matrix = {
+  title: 'Matrix',
+  author: 'wachowsky',
+  pages: 400,
+  read: true
+}
+
+
+booksArray = [hobbit, potter, matrix];
+
+console.log(booksArray[0])
+
+
