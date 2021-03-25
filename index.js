8695e329a4ff27f998d24eaaa653260728fd469e
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-25 19:39:19 -0300

    Merge codes

--- index.js
+++ index.js
@@ -73,4 +73,9 @@ function elementsLoop(arr) {
 elementsLoop(booksArray)
 
 
+// var person = prompt("Please enter your name", "Harry Potter");
 
+// if (person != null) {
+//   document.getElementById("demo").innerHTML =
+//   "Hello " + person + "! How are you today?";
+// }
