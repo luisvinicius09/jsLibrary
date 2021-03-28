17036b557a9af2e6250ee611f9c19f58ab6ad2ad
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-28 14:37:07 -0300

    Add delete button

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function Book(title, author, pages, read) {
   this.author = author
   this.pages = pages
   this.read = read
-  }
+}
 
 submitBtn = document.getElementById('submit').addEventListener('click', () => {
   addBookToLibrary();
@@ -48,18 +48,19 @@ function addBookToLibrary() {
   const read = document.querySelector('#read').value
   
   const book = new Book(title, author, pages, read);
-  console.log(book)
   myLibrary.push(book);
 }
 
 function elementsLoop(arr) {
-  let books = arr.map(book => {
+  let books = arr.map((book, index) => {
     return (
       `<tr>
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.pages}</td>
         <td>${book.read}</td>
+        <td><button>Toggle read</button></td>
+        <td><button type='button' onClick='deleteBook(${index})' >Delete</button></td>
       </tr>`
     )
   }).join('\n')
@@ -68,25 +69,15 @@ function elementsLoop(arr) {
 
 elementsLoop(myLibrary)
 
-// const addBtn = document.createElement('#addBtn')
-// addBtn.setAttribute('type', 'button');
-// addBtn.innerHTML = `${myLibrary.read ? 'Read' : 'Unread'}`
+function deleteBook(id) {
+  myLibrary.slice(id, 1)
+  elementsLoop(myLibrary)
+}
 
-// }); //target is the NODE ITSELF
-
-
-function showForm() { // eslint-disable-line no-unused-vars
+function showForm() {
   document.getElementById('formd').style.display = 'block';
-  
 }
 
 const addBtn = document.getElementById('addBtn')
 addBtn.setAttribute('type', 'button');
 addBtn.addEventListener('click', (e) => e.target.style.display = 'none');
-
-// var person = prompt("Please enter your name", "Harry Potter");
-
-// if (person != null) {
-//   document.getElementById("demo").innerHTML =
-//   "Hello " + person + "! How are you today?";
-// }
