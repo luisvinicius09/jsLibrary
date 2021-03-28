4fbafc3bde59081ef34998d69c70a5a3d08803d0
realisticattorney <g.axl.aquila@gmail.com>
2021-03-28 17:20:55 -0300

    Style delete button. Add screenshot. Fix style linters

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-let hobbit = {
+const hobbit = {
   title: 'The Hobbit',
   author: 'Tolkien',
   pages: 1200,
-  read: true
-}
+  read: true,
+};
 
-let potter = {
+const potter = {
   title: 'Harry Potter',
   author: 'some lady',
   pages: 600,
-  read: false
-}
+  read: true,
+};
 
-let matrix = {
+const matrix = {
   title: 'Matrix',
   author: 'wachowsky',
   pages: 400,
-  read: true
-}
+  read: true,
+};
 
-let rings = {
+const rings = {
   title: 'Lord of Rings',
   author: 'JRR Tokien',
   pages: 500,
-  read: true
-}
+  read: true,
+};
 
 myLibrary = [hobbit, potter, matrix, rings];
 
 
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 submitBtn = document.getElementById('submit').addEventListener('click', () => {
@@ -45,53 +45,50 @@ function addBookToLibrary() {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const pages = document.querySelector('#pages').value;
-  const read = document.querySelector('#read').value
-  
+  const read = document.querySelector('#read').value;
+
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
 
 function displayBooks(arr) {
-  let books = arr.map((book, index) => {
-    return (
-      `<tr>
+  const books = arr.map((book, index) => (
+    `<tr>
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.pages}</td>
-        <td class="has-text-centered"><button class="button is-small is-primary" id=${book.read} onClick='toggleRead(${index})' value="Read">${book.read}</button></td>
-        <td><button class='btn' type='button' onClick='deleteBook(${index})'>Delete</button></td>
+        <td><button class="btn" id=${book.read} onClick='toggleRead(${index})' value="Read">${book.read}</button></td>
+        <td><button class='btn btn-delete' type='button' onClick='deleteBook(${index})'>Delete</button></td>
       </tr>`
-    )
-  }).join('\n')
-  document.querySelector('tbody').innerHTML = books
+  )).join('\n');
+  document.querySelector('tbody').innerHTML = books;
 }
 
-displayBooks(myLibrary)
+displayBooks(myLibrary);
 
 function toggleRead(id) {
-  for(let i=0; i < myLibrary.length; i++) {
-    if(i === id) {
-      if(myLibrary[i].read) {
-        myLibrary[i].read = false
-        displayBooks(myLibrary)
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (i === id) {
+      if (myLibrary[i].read) {
+        myLibrary[i].read = false;
+        displayBooks(myLibrary);
       } else {
-        myLibrary[i].read = true
-        displayBooks(myLibrary)
+        myLibrary[i].read = true;
+        displayBooks(myLibrary);
       }
     }
-
   }
 }
 
 function deleteBook(id) {
-  myLibrary.splice(id, 1)
-  displayBooks(myLibrary)
+  myLibrary.splice(id, 1);
+  displayBooks(myLibrary);
 }
 
 function showForm() {
   document.getElementById('formd').style.display = 'block';
 }
 
-const addBtn = document.getElementById('addBtn')
+const addBtn = document.getElementById('addBtn');
 addBtn.setAttribute('type', 'button');
 addBtn.addEventListener('click', (e) => e.target.style.display = 'none');
