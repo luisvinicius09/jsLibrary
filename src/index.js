4bbdcb88c96d11ceeb300cfa98defe8544f98108
realisticattorney <g.axl.aquila@gmail.com>
2021-03-29 12:03:53 -0300

    Add class Books and constructor function

--- src/index.js
+++ src/index.js
@@ -29,11 +29,13 @@ const rings = {
 const myLibrary = [hobbit, potter, matrix, rings];
 
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 const addBookToLibrary = () => {
   const title = document.querySelector('#title').value;
